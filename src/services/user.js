4019db47eb52da3f50b2fb83db503b1485cc906e
joaovictorns <jvictor.neves20@gmail.com>
2026-05-25 14:35:28 -0300

    refactor: add 'sexo' field to funcionarios and clean up UserService methods

--- src/services/user.js
+++ src/services/user.js
@@ -1,39 +1,39 @@
 import Database from '@database';
 
 export default class UserService {
-  constructor() {
-    this.database = new Database();
-  }
+	constructor() {
+		this.database = new Database();
+	}
 
-  async createAdministrador() {
-    try {
-      return await this.database.createAdministrador();
-    } catch (error) {
-      throw new Error('Error creating administrador: ' + error.message);
-    }
-  }
+	async createAdministrador() {
+		try {
+			return await this.database.createAdministrador();
+		} catch (error) {
+			throw new Error('Error creating administrador: ' + error.message);
+		}
+	}
 
-  async createGerente() {
-    try {
-      return await this.database.createGerente();
-    } catch (error) {
-      throw new Error('Error creating gerente: ' + error.message);
-    }
-  }
+	async createGerente() {
+		try {
+			return await this.database.createGerente();
+		} catch (error) {
+			throw new Error('Error creating gerente: ' + error.message);
+		}
+	}
 
-  async createFuncionario() {
-    try {
-      return await this.database.createFuncionario();
-    } catch (error) {
-      throw new Error('Error creating funcionario: ' + error.message);
-    }
-  }
+	async createFuncionario() {
+		try {
+			return await this.database.createFuncionario();
+		} catch (error) {
+			throw new Error('Error creating funcionario: ' + error.message);
+		}
+	}
 
-  async createUsersWithRoles() {
-    try {
-      return await this.database.createUsersWithRoles();
-    } catch (error) {
-      throw new Error('Error executing user method: ' + error.message);
-    }
-  }
+	async createUsersWithRoles() {
+		try {
+			return await this.database.createUsersWithRoles();
+		} catch (error) {
+			throw new Error('Error executing user method: ' + error.message);
+		}
+	}
 }
